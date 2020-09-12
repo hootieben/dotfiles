259d24947b263659caca4196998f7d44c98288df
Ben Sykes <hootieben@gmail.com>
2020-09-12 12:02:46 -0500

    Only run if nvim is found

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#!/usr/bin/env bash
+
+if hash nvim 2>/dev/null; then
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim --headless +"PlugInstall" +qa
+  nvim --headless +"PlugInstall" +qa
+else
+  echo "NVIM Executable Not Found"
+  exit 0
+fi
