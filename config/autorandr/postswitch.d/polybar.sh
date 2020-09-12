0ea69e80c88b9e628dbca2df440f2514e658ac9a
Ben Sykes <hootieben@gmail.com>
2020-09-12 13:12:19 -0500

    Shellcheck fix

--- config/autorandr/postswitch.d/polybar.sh
+++ config/autorandr/postswitch.d/polybar.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if $(systemctl is-active --quiet --user polybar); then
+if systemctl is-active --quiet --user polybar; then
     systemctl --user restart polybar
-    if $(dropbox running); then
+    if dropbox running; then
         dropbox stop && dropbox start
     fi
 fi
