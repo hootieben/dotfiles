d22954e71f75eec21ec65a69e2c6df893b3c2733
Ben Sykes <hootieben@gmail.com>
2020-09-12 12:50:38 -0500

    Only run if ZSH is installed

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -d ~/.oh-my-zsh ]]; then
-    echo "Oh My ZSH Already Installed"
-    exit 0
-else
+if hash zsh 2>/dev/null; then
+  if [[ -d ~/.oh-my-zsh ]]; then
+      echo "Oh My ZSH Already Installed"
+      exit 0
+  else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" --unattended
+  fi
+else
+  echo "ZSH Not Found. Exiting Oh My ZSH Install"
+  exit 0
 fi
