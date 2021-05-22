4362396bce1799b193931a32d56b5ce426651db6
Ben Sykes <hootieben@gmail.com>
2021-05-22 11:43:37 -0500

    Add output to list which config is being run

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@ git submodule update --init --recursive "${DOTBOT_DIR}"
 # "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
 for conf in ${DEFAULT_CONFIG_PREFIX} ${ARCH}; do
   if [[ -f "${conf}${CONFIG_SUFFIX}" ]]; then
+    echo "Running ${conf} configuration"
     "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${conf}${CONFIG_SUFFIX}"
   fi
 done
